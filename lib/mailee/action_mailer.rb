f0bb79f3d59ed6bf70031742e681137ed40f5e80
Daniel Weinmann <danielweinmann@gmail.com>
2011-06-22 20:31:52 -0700

    Changing to_s to join because it was returning an array as string

--- lib/mailee/action_mailer.rb
+++ lib/mailee/action_mailer.rb
@@ -27,7 +27,7 @@ module Mailee
     def initialize config
     end
     def deliver! mail
-      from_name = mail.header['from'].to_s.scan(/(.+?) <.+?>$/).to_s      
+      from_name = mail.header['from'].to_s.scan(/(.+?) <.+?>$/).join      
       message = Mailee::Message.create :title => mail.subject, :subject => mail.subject, :from_name => from_name, :from_email => mail.from.first, :emails => mail.to.join(' '), :html => mail.body.to_s
       result = message.ready(mail.date)
       mail.instance_eval{ self.class.send('attr_accessor', :mailee_message); self.mailee_message = message }
